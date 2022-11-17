7cba7b1660835485a62152249dda517c68ed8318
nicoContigliani <nico.contigliani@gmail.com>
2022-11-17 17:34:17 -0300

    new

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,15 @@ import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController, TasksController],
+  controllers: [
+    AppController,
+    TasksController,
+    //}MongooseModule.forRoot('mongodb://localhost:27017/nestdb'),
+  ],
   providers: [AppService, TasksService],
 })
 export class AppModule {}
